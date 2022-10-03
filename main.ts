ee4b7bdaa12944405a39557a8f91a0f07aad9583
agatavaraujo <113917707+agatavaraujo@users.noreply.github.com>
2022-10-03 11:40:31 -0300

    Atualizar 'main.blocks, main.ts'

--- main.ts
+++ main.ts
@@ -1,3 +1,45 @@
+let cont2 = 0
+let cont1 = 0
+input.onPinPressed(TouchPin.P0, function () {
+    basic.showNumber(3)
+    basic.showNumber(2)
+    basic.showNumber(1)
+    basic.clearScreen()
+    basic.pause(randint(1000, 5000))
+    basic.showLeds(`
+        # # # # #
+        # . # . #
+        # # . # #
+        # . # . #
+        # # # # #
+        `)
+    basic.pause(500)
+    basic.clearScreen()
+})
+input.onPinPressed(TouchPin.P2, function () {
+    cont2 += 1
+    basic.showLeds(`
+        . . # # #
+        . . # # #
+        . . # # #
+        . . # # #
+        . . # # #
+        `)
+    basic.pause(1000)
+    basic.clearScreen()
+})
+input.onPinPressed(TouchPin.P1, function () {
+    cont1 += 1
+    basic.showLeds(`
+        # # # . .
+        # # # . .
+        # # # . .
+        # # # . .
+        # # # . .
+        `)
+    basic.pause(1000)
+    basic.clearScreen()
+})
 basic.forever(function () {
 	
 })
